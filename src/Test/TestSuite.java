aafff430757985c5e74d4e47df9e5c1f898b308e
ChamesBond <85110710+ChamesBond@users.noreply.github.com>
2022-01-18 17:02:32 +0100

    Delete TestSuite.java

--- src/Test/TestSuite.java
+++
@@ -1,9 +0,0 @@
-package Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import Test.Model.Magazyn.MagazynTest;
-import Test.Model.Magazyn.ProduktTest;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({MagazynTest.class, ProduktTest.class, AplikacjaTest.class})
-public class TestSuite { }
